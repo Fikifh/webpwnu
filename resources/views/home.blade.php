9b5a730db8c862fb2b1a183f696a9bafab556311
Fiki Firmansyah <fikiirmansyah@gmail.com>
2020-03-15 22:29:44 +0700

    initial commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,8 @@
 <head>
     <title>PWNU</title>
     <meta charset="UTF-8">
-    <meta name="description" content="SolMusic HTML Template">
-    <meta name="keywords" content="music, html">
+    <meta name="description" content="Website Resmi Pengurus Wilayah Nahdlatul Ulama.">
+    <meta name="keywords" content="pwnu, nu, nahdatul ulama, nkri">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Favicon -->
